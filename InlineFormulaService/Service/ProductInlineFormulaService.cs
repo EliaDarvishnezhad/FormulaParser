433fde4feb74d1962f118c4acfdb1f6a9fb07a00
Elia Darvishnezhad <53391759+EliaDarvishnezhad@users.noreply.github.com>
2020-11-24 11:29:30 +0330

    Fix Incorrect Using

--- InlineFormulaService/Service/ProductInlineFormulaService.cs
+++ InlineFormulaService/Service/ProductInlineFormulaService.cs
@@ -1,71 +1,70 @@
 ﻿using Septa.PayamGostar.Domain.Dto.BaseInfo.InlineFormula;
 using Septa.PayamGostar.Domain.Service.ProductManagement;
-using SeptaNSFCore.Helper.ExtensionMethod;
 using System;
 using System.Collections.Generic;
 
 namespace Septa.PayamGostar.CrmService.ProductManagement
 {
-    public class ProductInlineFormulaService : IProductInlineFormulaService
-    {
-        public const string PlusSign = "+";
-        public const string MinusSign = "-";
-        public const string AsteriskSign = "*";
-        public const string SlashSign = "/";
+	public class ProductInlineFormulaService : IProductInlineFormulaService
+	{
+		public const string PlusSign = "+";
+		public const string MinusSign = "-";
+		public const string AsteriskSign = "*";
+		public const string SlashSign = "/";
 
-        private string[] ReservedOperatorSigns
-        {
-            get
-            {
-                return new string[] { PlusSign, MinusSign, AsteriskSign, SlashSign };
-            }
-        }
+		private string[] ReservedOperatorSigns
+		{
+			get
+			{
+				return new string[] { PlusSign, MinusSign, AsteriskSign, SlashSign };
+			}
+		}
 
-        public ProductInlineFormulaService()
-        {
+		public ProductInlineFormulaService()
+		{
 
-        }
+		}
 
-        #region Service Methods
+		#region Service Methods
 
-        public decimal CalculateFormula(string formula, Dictionary<string, decimal> variableValues)
-        {
-            throw new NotImplementedException();
-        }
+		public decimal CalculateFormula(string formula, Dictionary<string, decimal> variableValues)
+		{
+			throw new NotImplementedException();
+		}
 
-        public decimal CalculateFormula(IEnumerable<InlineFormulaEntryDto> inlineFormulaEntries, Dictionary<string, decimal> variableValues)
-        {
-            throw new NotImplementedException();
-        }
+		public decimal CalculateFormula(IEnumerable<InlineFormulaEntryDto> inlineFormulaEntries, Dictionary<string, decimal> variableValues)
+		{
+			throw new NotImplementedException();
+		}
 
-        public IEnumerable<OperandInlineFormulaEntryInfoDto> GetListOfUsedVariablesInFormula(string formula)
-        {
-            throw new NotImplementedException();
-        }
+		public IEnumerable<OperandInlineFormulaEntryInfoDto> GetListOfUsedVariablesInFormula(string formula)
+		{
+			throw new NotImplementedException();
+		}
 
-        public bool ValidateFormulaSyntax(string formula, IEnumerable<string> supportedVariableCollection)
-        {
+		public bool ValidateFormulaSyntax(string formula, IEnumerable<string> supportedVariableCollection)
+		{
 
-            throw new NotImplementedException();
-        }
+			throw new NotImplementedException();
+		}
 
-        #endregion
+		#endregion
 
-        #region Private Methods
+		#region Private Methods
 
 
-        private List<InlineFormulaEntryDto> ParseFormula(string formula)
-        {
-            if (formula.HasValue())
-            {
-                var toReturn = new List<InlineFormulaEntryDto>();
+		private List<InlineFormulaEntryDto> ParseFormula(string formula)
+		{
+			if (!string.IsNullOrEmpty(formula))
+			{
+				var toReturn = new List<InlineFormulaEntryDto>();
 
-                return toReturn;
-            }
-            else
-                return null;
-        }
+				return toReturn;
+			}
+			else
+				return null;
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
