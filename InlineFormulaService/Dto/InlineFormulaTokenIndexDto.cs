7e2f2181b8d8d711e542c9ca17249e7cef7b1447
Elia Darvishnezhad <53391759+EliaDarvishnezhad@users.noreply.github.com>
2020-11-30 13:03:08 +0330

    Some Files Renamed

---
+++ InlineFormulaService/Dto/InlineFormulaTokenIndexDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Septa.PayamGostar.Domain.Dto.BaseInfo.InlineFormula
+{
+	public class InlineFormulaTokenIndexDto
+	{
+		public InlineFormulaTokenIndexDto(string token, int index)
+		{
+			this.Token = token;
+			this.Index = index;
+		}
+
+		public string Token { get; set; }
+		public int Index { get; set; }
+	}
+}
